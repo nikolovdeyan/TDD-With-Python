0425ffbd729d8ed356ba2ee6507f56baab0edc48
nikolovdeyan <deyan.nikolov@gmail.com>
2017-11-12 08:43:35 +0200

    201711120843 | Hack FT runner to be able to test staging

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,4 +1,5 @@
 #pylint: disable=missing-docstring, invalid-name, line-too-long
+import os
 import time
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
@@ -12,6 +13,10 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
+
 
     def tearDown(self):
         self.browser.refresh()
