752e45505ff4b93cbea07b21d70af35f1ca42671
nikolovdeyan <deyan.nikolov@gmail.com>
2017-11-15 14:58:33 +0200

    201711151458 | Refactor list view to handle new item POSTs

--- lists/models.py
+++ lists/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 
 class List(models.Model):
+    objects = models.Manager()
     pass
 
 
 class Item(models.Model):
+    objects = models.Manager()
     text = models.TextField(default='')
     list = models.ForeignKey(List, default=None)
